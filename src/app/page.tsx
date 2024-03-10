adc676086154499af1f6681dc546be9ce49db682
CongSanhLe <congsanhle1999@gmail.com>
2024-03-10 14:57:33 +0700

    [0305] signIn/Up func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="">
-      <UserButton />
+      <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
