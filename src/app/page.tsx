a112fed1a75b03271d8894ef19d9df70c02ef3b2
CongSanhLe <congsanhle1999@gmail.com>
2024-03-10 15:17:05 +0700

    [0306] current user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import { Button, Flex } from "antd";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const logInUserData = await currentUser();
+  let email = "";
+  if (logInUserData?.emailAddresses && logInUserData?.emailAddresses.length) {
+    email = logInUserData.emailAddresses[0].emailAddress;
+  }
+
   return (
-    <div className="">
-      <UserButton afterSignOutUrl="/sign-in" />
+    <div className="p-10">
+      <div className="flex flex-col gap-3">
+        <UserButton afterSignOutUrl="/sign-in" />
+        <span>First Name: {logInUserData?.firstName}</span>
+        <span>Last Name: {logInUserData?.lastName}</span>
+        <span>User Name: {logInUserData?.username}</span>
+        <span>Email: {email}</span>
+      </div>
     </div>
   );
 }
