c60ac78f87fd1f00c9ef5488551b25507634849c
CongSanhLe <congsanhle1999@gmail.com>
2024-03-13 14:30:55 +0700

    [0802] chat n message model

---
+++ src/models/chat-model.ts
@@ -0,0 +1,51 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    users: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "users",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "messages",
+    },
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+    groupName: {
+      type: String,
+      default: "",
+    },
+    groupProfilePicture: {
+      type: String,
+      default: "",
+    },
+    groupBio: {
+      type: String,
+      default: "",
+    },
+    groupAdmins: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "users",
+    },
+    unreadCounts: {
+      type: Object,
+      default: {},
+    },
+  },
+  { timestamps: true }
+);
+
+// check if model is already compiled, if YES delete it
+if (mongoose.models && mongoose.models["chats"]) {
+  mongoose.deleteModel("chats");
+}
+
+const ChatModel = mongoose.model("users", chatSchema);
+export default ChatModel;
