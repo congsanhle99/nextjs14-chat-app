1cbe71cbf3719b0fc06b782c481c62ccf0777753
CongSanhLe <congsanhle1999@gmail.com>
2024-03-13 14:31:48 +0700

    [0803] chat n message interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -8,3 +8,29 @@ export interface UserType {
   createAt: string;
   updateAt: string;
 }
+
+export interface ChatType {
+  _id: string;
+  users: UserType[];
+  createdBy: UserType;
+  lastMessage: MessageType;
+  isGroupChat: boolean;
+  groupName: string;
+  groupProfilePicture: string;
+  groupBio: string;
+  groupAdmins: UserType[];
+  unreadCounts: any;
+  createAt: string;
+  updateAt: string;
+}
+
+export interface MessageType {
+  _id: string;
+  chat: ChatType;
+  sender: UserType;
+  text: string;
+  image: string;
+  readBy: UserType[];
+  createAt: string;
+  updateAt: string;
+}
